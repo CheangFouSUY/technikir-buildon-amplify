a7d3a504c343dc86470507e4f46834a97227af5b
cheangfousuy <thecounterfeitcat@gmail.com>
2021-05-22 14:21:58 +0700

    inputing form and fetching from graphql

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,6 +1,201 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const createDestinationAcc = /* GraphQL */ `
+  mutation CreateDestinationAcc(
+    $input: CreateDestinationAccInput!
+    $condition: ModelDestinationAccConditionInput
+  ) {
+    createDestinationAcc(input: $input, condition: $condition) {
+      id
+      first_name
+      last_name
+      acc_num
+      _version
+      _deleted
+      _lastChangedAt
+      createdAt
+      updatedAt
+      DestinationAccTransactions {
+        nextToken
+        startedAt
+      }
+    }
+  }
+`;
+export const updateDestinationAcc = /* GraphQL */ `
+  mutation UpdateDestinationAcc(
+    $input: UpdateDestinationAccInput!
+    $condition: ModelDestinationAccConditionInput
+  ) {
+    updateDestinationAcc(input: $input, condition: $condition) {
+      id
+      first_name
+      last_name
+      acc_num
+      _version
+      _deleted
+      _lastChangedAt
+      createdAt
+      updatedAt
+      DestinationAccTransactions {
+        nextToken
+        startedAt
+      }
+    }
+  }
+`;
+export const deleteDestinationAcc = /* GraphQL */ `
+  mutation DeleteDestinationAcc(
+    $input: DeleteDestinationAccInput!
+    $condition: ModelDestinationAccConditionInput
+  ) {
+    deleteDestinationAcc(input: $input, condition: $condition) {
+      id
+      first_name
+      last_name
+      acc_num
+      _version
+      _deleted
+      _lastChangedAt
+      createdAt
+      updatedAt
+      DestinationAccTransactions {
+        nextToken
+        startedAt
+      }
+    }
+  }
+`;
+export const createTransactionType = /* GraphQL */ `
+  mutation CreateTransactionType(
+    $input: CreateTransactionTypeInput!
+    $condition: ModelTransactionTypeConditionInput
+  ) {
+    createTransactionType(input: $input, condition: $condition) {
+      id
+      transac_type_id
+      transac_type_name
+      _version
+      _deleted
+      _lastChangedAt
+      createdAt
+      updatedAt
+      Transactions {
+        nextToken
+        startedAt
+      }
+    }
+  }
+`;
+export const updateTransactionType = /* GraphQL */ `
+  mutation UpdateTransactionType(
+    $input: UpdateTransactionTypeInput!
+    $condition: ModelTransactionTypeConditionInput
+  ) {
+    updateTransactionType(input: $input, condition: $condition) {
+      id
+      transac_type_id
+      transac_type_name
+      _version
+      _deleted
+      _lastChangedAt
+      createdAt
+      updatedAt
+      Transactions {
+        nextToken
+        startedAt
+      }
+    }
+  }
+`;
+export const deleteTransactionType = /* GraphQL */ `
+  mutation DeleteTransactionType(
+    $input: DeleteTransactionTypeInput!
+    $condition: ModelTransactionTypeConditionInput
+  ) {
+    deleteTransactionType(input: $input, condition: $condition) {
+      id
+      transac_type_id
+      transac_type_name
+      _version
+      _deleted
+      _lastChangedAt
+      createdAt
+      updatedAt
+      Transactions {
+        nextToken
+        startedAt
+      }
+    }
+  }
+`;
+export const createSourceAcc = /* GraphQL */ `
+  mutation CreateSourceAcc(
+    $input: CreateSourceAccInput!
+    $condition: ModelSourceAccConditionInput
+  ) {
+    createSourceAcc(input: $input, condition: $condition) {
+      id
+      first_name
+      last_name
+      acc_num
+      _version
+      _deleted
+      _lastChangedAt
+      createdAt
+      updatedAt
+      Transactions {
+        nextToken
+        startedAt
+      }
+    }
+  }
+`;
+export const updateSourceAcc = /* GraphQL */ `
+  mutation UpdateSourceAcc(
+    $input: UpdateSourceAccInput!
+    $condition: ModelSourceAccConditionInput
+  ) {
+    updateSourceAcc(input: $input, condition: $condition) {
+      id
+      first_name
+      last_name
+      acc_num
+      _version
+      _deleted
+      _lastChangedAt
+      createdAt
+      updatedAt
+      Transactions {
+        nextToken
+        startedAt
+      }
+    }
+  }
+`;
+export const deleteSourceAcc = /* GraphQL */ `
+  mutation DeleteSourceAcc(
+    $input: DeleteSourceAccInput!
+    $condition: ModelSourceAccConditionInput
+  ) {
+    deleteSourceAcc(input: $input, condition: $condition) {
+      id
+      first_name
+      last_name
+      acc_num
+      _version
+      _deleted
+      _lastChangedAt
+      createdAt
+      updatedAt
+      Transactions {
+        nextToken
+        startedAt
+      }
+    }
+  }
+`;
 export const createTransaction = /* GraphQL */ `
   mutation CreateTransaction(
     $input: CreateTransactionInput!
@@ -11,21 +206,20 @@ export const createTransaction = /* GraphQL */ `
       type
       currency
       amount
-      source_info {
-        account_id
-        first_name
-        last_name
-      }
-      destination_info {
-        account_id
-        first_name
-        last_name
-      }
       swift_code
       beneficiary_bank
       purpose_of_transfer
+      sourceaccountID
+      transactiontypeID
+      _version
+      _deleted
+      _lastChangedAt
       createdAt
       updatedAt
+      destinationaccs {
+        nextToken
+        startedAt
+      }
     }
   }
 `;
@@ -39,21 +233,20 @@ export const updateTransaction = /* GraphQL */ `
       type
       currency
       amount
-      source_info {
-        account_id
-        first_name
-        last_name
-      }
-      destination_info {
-        account_id
-        first_name
-        last_name
-      }
       swift_code
       beneficiary_bank
       purpose_of_transfer
+      sourceaccountID
+      transactiontypeID
+      _version
+      _deleted
+      _lastChangedAt
       createdAt
       updatedAt
+      destinationaccs {
+        nextToken
+        startedAt
+      }
     }
   }
 `;
@@ -67,21 +260,152 @@ export const deleteTransaction = /* GraphQL */ `
       type
       currency
       amount
-      source_info {
-        account_id
-        first_name
-        last_name
-      }
-      destination_info {
-        account_id
-        first_name
-        last_name
-      }
       swift_code
       beneficiary_bank
       purpose_of_transfer
+      sourceaccountID
+      transactiontypeID
+      _version
+      _deleted
+      _lastChangedAt
       createdAt
       updatedAt
+      destinationaccs {
+        nextToken
+        startedAt
+      }
+    }
+  }
+`;
+export const createDestinationAccTransaction = /* GraphQL */ `
+  mutation CreateDestinationAccTransaction(
+    $input: CreateDestinationAccTransactionInput!
+    $condition: ModelDestinationAccTransactionConditionInput
+  ) {
+    createDestinationAccTransaction(input: $input, condition: $condition) {
+      id
+      destinationaccID
+      transactionID
+      _version
+      _deleted
+      _lastChangedAt
+      createdAt
+      updatedAt
+      destinationacc {
+        id
+        first_name
+        last_name
+        acc_num
+        _version
+        _deleted
+        _lastChangedAt
+        createdAt
+        updatedAt
+      }
+      transaction {
+        id
+        type
+        currency
+        amount
+        swift_code
+        beneficiary_bank
+        purpose_of_transfer
+        sourceaccountID
+        transactiontypeID
+        _version
+        _deleted
+        _lastChangedAt
+        createdAt
+        updatedAt
+      }
+    }
+  }
+`;
+export const updateDestinationAccTransaction = /* GraphQL */ `
+  mutation UpdateDestinationAccTransaction(
+    $input: UpdateDestinationAccTransactionInput!
+    $condition: ModelDestinationAccTransactionConditionInput
+  ) {
+    updateDestinationAccTransaction(input: $input, condition: $condition) {
+      id
+      destinationaccID
+      transactionID
+      _version
+      _deleted
+      _lastChangedAt
+      createdAt
+      updatedAt
+      destinationacc {
+        id
+        first_name
+        last_name
+        acc_num
+        _version
+        _deleted
+        _lastChangedAt
+        createdAt
+        updatedAt
+      }
+      transaction {
+        id
+        type
+        currency
+        amount
+        swift_code
+        beneficiary_bank
+        purpose_of_transfer
+        sourceaccountID
+        transactiontypeID
+        _version
+        _deleted
+        _lastChangedAt
+        createdAt
+        updatedAt
+      }
+    }
+  }
+`;
+export const deleteDestinationAccTransaction = /* GraphQL */ `
+  mutation DeleteDestinationAccTransaction(
+    $input: DeleteDestinationAccTransactionInput!
+    $condition: ModelDestinationAccTransactionConditionInput
+  ) {
+    deleteDestinationAccTransaction(input: $input, condition: $condition) {
+      id
+      destinationaccID
+      transactionID
+      _version
+      _deleted
+      _lastChangedAt
+      createdAt
+      updatedAt
+      destinationacc {
+        id
+        first_name
+        last_name
+        acc_num
+        _version
+        _deleted
+        _lastChangedAt
+        createdAt
+        updatedAt
+      }
+      transaction {
+        id
+        type
+        currency
+        amount
+        swift_code
+        beneficiary_bank
+        purpose_of_transfer
+        sourceaccountID
+        transactiontypeID
+        _version
+        _deleted
+        _lastChangedAt
+        createdAt
+        updatedAt
+      }
     }
   }
 `;
